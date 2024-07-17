e6c186c16bcc56f15952ae58216e8bbe16a09116
anabanadvali <bekabitarashvili@gmail.com>
2024-07-17 11:31:16 +0400

    Forms + Flash

--- app.py
+++ app.py
@@ -1,11 +1,36 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, redirect, url_for, flash
+from forms import RegistrationForm, LoginForm
+
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '879asdfnjsdf989jsdffsdsdf546'
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('index'))
+    return render_template('register.html', form=form)
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'bitara@posta.ge' and form.password.data == '12345678':
+            flash(f'You have been logged in!', 'success')
+            return redirect(url_for('index'))
+        else:
+            flash(f'Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', form=form)
 
 
 @app.errorhandler(404)
